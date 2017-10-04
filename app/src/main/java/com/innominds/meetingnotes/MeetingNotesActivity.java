8ab4e7c53f9f1748c9e1d8cafc98635c163821ca
sgarimella@innominds.com <sgarimella@innominds.com>
2017-10-04 12:32:04 +0530

    first commit

---
+++ app/src/main/java/com/innominds/meetingnotes/MeetingNotesActivity.java
@@ -0,0 +1,13 @@
+package com.innominds.meetingnotes;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MeetingNotesActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_meeting_notes);
+    }
+}
